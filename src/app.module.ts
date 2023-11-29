1b31436e9fc91cbc7230cb3d6dab8fd454b02e95
DevanshDW <devanshpatel9300@gmail.com>
2023-11-29 17:50:10 -0400

    first

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    UsersModule,
+  ],
+   controllers: [AppController],
+   providers: [AppService],
+})
+export class AppModule {}
