79257bf76d3533622812e1400f3bcd276073d425
DevanshDW <devanshpatel9300@gmail.com>
2023-11-29 18:09:20 -0400

    Joi validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,26 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema:Joi.object({
+        PORT: Joi.number().required(),
+        MONGODB_URI: Joi.string().required(),
+      })
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
     }),
     UsersModule,
+    DatabaseModule,
+
   ],
    controllers: [AppController],
    providers: [AppService],
